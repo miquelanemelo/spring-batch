6a3c64b676dcdb9dbad8a80c4080bc9ac35cf92e
Miquelane <miquelanepmelo@gmail.com>
2022-07-27 16:25:10 -0300

    primeiro commit

---
+++ src/main/java/com/udemy/primeiroprojetospringsatch/PrimeiroprojetospringsatchApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.primeiroprojetospringsatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroprojetospringsatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroprojetospringsatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy/primeiroprojetospringsatch/PrimeiroprojetospringsatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.primeiroprojetospringsatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroprojetospringsatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
